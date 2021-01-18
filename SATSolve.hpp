b1259f28292ef07b35ade1ae8ac5e092fe52c0b7
Eric Moyer <compu.moyer@gmail.com>
2021-01-18 01:52:08 -0500

    Make main read from stdin and echo the instance

--- SATSolve.hpp
+++ SATSolve.hpp
@@ -1,3 +1,44 @@
-struct Literal {
+#pragma once
+#include <map>
+#include <string>
+#include <vector>
+
+struct LiteralCode {
     unsigned int value;
 };
+
+using Clause = std::vector<LiteralCode>;
+
+struct SATInstance {
+    /// variables[index_of_variable] = variable_name
+    std::vector<std::string> variables;
+    /// variable_table[variable_name] = index_of_variable
+    std::map<std::string, unsigned> variable_table;
+    std::vector<Clause> clauses;
+};
+
+enum class LiteralState { normal = 0, negated = 1 };
+
+void add_literal_to_clause(const std::string& variable,
+                           const LiteralState state, Clause& clause,
+                           SATInstance& instance) {
+    unsigned negated = static_cast<unsigned>(state);
+
+    auto& literal_codes = instance.variable_table;
+    unsigned variable_code;
+    const auto& code_location = literal_codes.find(variable);
+    if (code_location == literal_codes.end()) {
+        variable_code = instance.variables.size();
+        literal_codes[variable] = variable_code;
+        instance.variables.push_back(variable);
+    } else {
+        variable_code = code_location->second;
+    }
+    LiteralCode encoded_literal;
+    encoded_literal.value = variable_code << 1 | negated;
+    clause.push_back(encoded_literal);
+}
+
+inline void add_clause_to_instance(Clause& clause, SATInstance& instance) {
+    instance.clauses.push_back(clause);
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,93 @@
+#include <algorithm>
+#include <cctype>
 #include <iostream>
+#include <sstream>
+#include "SATSolve.hpp"
+
+void parse_line_and_add_clause(SATInstance& instance, const std::string& line) {
+    std::stringstream line_stream(line);
+    std::string literal_str;
+
+    Clause clause;
+    while (std::getline(line_stream, literal_str, ' ')) {
+        if (literal_str.size() == 0) {
+            continue;
+        }
+
+        // The variable is negated if it starts with ~
+        LiteralState state = literal_str[0] == '~' ? LiteralState::negated
+                                                   : LiteralState::normal;
+
+        // Remove ~ if necessary
+        std::string variable;
+        auto start = state == LiteralState::negated
+                         ? std::next(literal_str.begin())
+                         : literal_str.begin();
+        std::copy(start, literal_str.end(), std::back_inserter(variable));
+
+        add_literal_to_clause(variable, state, clause, instance);
+    }
+    add_clause_to_instance(clause, instance);
+}
+
+// Strip whitespace from both ends of line
+// Note that this can break on UTF-8 input ...
+void strip_whitespace(std::string& line) {
+    while (not line.empty() and std::isspace(line.back())) {
+        line.pop_back();
+    }
+    while (not line.empty() and std::isspace(line.front())) {
+        line.erase(line.begin());
+    }
+}
+
+SATInstance sat_instance_from_file(std::istream& file) {
+    SATInstance instance;
+    std::string line;
+    while (std::getline(file, line, '\n')) {
+        strip_whitespace(line);
+        // Skip blank lines and comments
+        if (line.size() > 0 and line[0] != '#') {
+            parse_line_and_add_clause(instance, line);
+        }
+    }
+    return instance;
+}
+
+std::string literal_to_string(const LiteralCode l,
+                              const SATInstance& instance) {
+    std::string s{l.value & 1 ? "~" : ""};
+    s += instance.variables[l.value >> 1];
+    return s;
+}
+
+std::string clause_to_string(const Clause& clause,
+                             const SATInstance& instance) {
+    std::string s;
+    auto cur = clause.begin();
+    if (cur == clause.end()) {
+        return s;
+    }
+    s += literal_to_string(*cur++, instance);
+    while (cur != clause.end()) {
+        s += " ";
+        s += literal_to_string(*cur++, instance);
+    }
+    return s;
+}
+
+std::ostream& operator<<(std::ostream& o, const SATInstance& i) {
+    for (const auto& c : i.clauses) {
+        if (not(o << clause_to_string(c, i) << std::endl)) {
+            break;
+        };
+    }
+    return o;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto instance = sat_instance_from_file(std::cin);
+    std::cout << "Read instance!" << std::endl;
+    std::cout << instance << std::endl;
     return 0;
 }
